fbb3f6e5b22554482017ac72e8760075e67ca480
mac <mac@macs-MacBook-Air.local>
2022-11-08 15:02:04 +0800

    .

--- init.lua
+++ init.lua
@@ -1,19 +1,85 @@
--- minimal hammerspoon config
--- created at 2022-05-01
+-- hammerspoon config example
+-- date: 2022-11-08
 
 local obj = {}
 obj._metadata = {
-    name = 'My Minimal Hammerspoon Config',
-    version = '1.2.3-0503',
-    author = 'anonymous',
+    name = 'hammerspoon config example',
+    version = '1.0',
+    author = '',
     homepage = '',
     license = 'MIT - https://opensource.org/licenses/MIT',
 }
 
-obj.cfg = {
-    autoReloadConfig = true,
-    showClock = true,
-    appmap = {
+obj.cfg = {}
+obj.map = {
+    reloadConfig = { { 'ctrl', 'cmd' }, 'r' },
+    toggleHelp = { { 'ctrl', 'alt' }, '/' },
+    windowManager = { { 'alt', 'cmd' }, 'w' },
+    toggleClock = {{'ctrl', 'cmd'}, 't'},
+}
+obj.spoons = {}
+
+----
+function obj:init()
+    for k, v in pairs(obj._metadata) do obj[k] = v end
+    for _, v in ipairs(obj.spoons) do hs.loadSpoon(v) end
+
+    obj.watchers = {}
+    obj.timers = {}
+    obj.canvases = {}
+    return self
+end
+
+function obj:start()
+    obj:_setupReloadConfig()
+    obj:_setupAppLauncher()
+    obj:_setupWindowManager()
+    obj:_toggleClock(true)
+    return self
+end
+
+function obj:stop()
+    return self
+end
+
+function obj:bindHotkeys(mapping)
+    local def = {
+        toggleHelp = function()
+            obj:_toggleHelp()
+        end,
+        toggleClock = function ()
+            obj:_toggleClock()
+        end,
+    }
+    for k, v in pairs(mapping) do
+        if def[k] then
+            hs.hotkey.bind(v[1], v[2], def[k])
+        end
+    end
+    return self
+end
+
+---
+function obj:_setupReloadConfig()
+    local function notifyAndReload()
+        hs.notify.new({ title = "Hammerspoon", informativeText = "Config reloaded." }):send()
+        hs.reload()
+    end
+
+    local cfgWatcher = obj.watchers['cfgWatcher']
+    if not cfgWatcher or not cfgWatcher:running() then
+        cfgWatcher = hs.pathwatcher.new(hs.configdir, notifyAndReload)
+        obj.watchers['cfgWatcher'] = cfgWatcher
+        cfgWatcher:start()
+    end
+
+    keys = obj.map['reloadConfig']
+    hs.hotkey.bind(keys[1], keys[2], notifyAndReload)
+end
+
+function obj:_setupAppLauncher(keymap, modifiers)
+    local modifiers = modifiers or 'ctrl+alt'
+    local keymap = keymap or {
         a = 'App Store',
         b = 'Safari',
         c = 'Visual Studio Code',
@@ -42,75 +108,106 @@ obj.cfg = {
         x = 'XMind',
         y = 'Mail',
         --z = 'System Preferences'
-    },
-    keymap = {
-        appLauncher = {'alt', 'a'},
-        help = {'alt', '/'}
-    },
-    spoons = {
-        -- 'SPoonInstall',
-        -- 'KSheet',
-        -- 'Calendar',
-        -- 'HCalendar',
-        -- 'ClipboardTool',
-        -- 'WinWin',
-        -- 'CountDown',
-        -- 'FnMate',
     }
-}
 
-
---========== methods ==========--
-function obj:init()
-    obj.daemons = {}
-    obj.canvases = {}
-
-    for k,v in pairs(obj._metadata) do obj[k] = v end
-    for _,v in ipairs(obj.cfg.spoons) do hs.loadSpoon(v) end
-    return self
+    for k, v in pairs(keymap) do
+        hs.hotkey.bind(modifiers, k, function()
+            hs.application.launchOrFocus(v)
+        end)
+        -- update obj.map
+        obj.map[v] = { modifiers, k }
+    end
 end
 
-function obj:start()
-    if obj.cfg['autoReloadConfig'] then obj:_setupReloadConfig() end
-    if obj.cfg['showClock'] then obj:_toggleClock() end
-    obj:_setupAppLauncher()
-    return self
-end
+function obj:_setupWindowManager()
+    local id = 'windowManager'
+    local keys = obj.map[id]
+    local cmodal = hs.hotkey.modal.new(keys[1], keys[2])
 
-function obj:stop()
-    return self
-end
+    function cmodal:entered() hs.alert(string.format('Entered %s mode', id)) end
 
-function obj:bindHotkeys(mapping)
-    local def = {
-        help = function () obj:_toggleHelp() end
+    function cmodal:exited() hs.alert(string.format('Exited %s mode', id)) end
+
+    cmodal:bind('', 'escape', function()
+        cmodal:exit()
+    end)
+
+    local function move(direction, step)
+        local step = step or 10
+        local w = hs.window.focusedWindow()
+        local f = w:frame()
+
+        if direction == 'up' then
+            f.y = f.y - step
+        end
+        if direction == 'down' then
+            f.y = f.y + step
+        end
+        if direction == 'left' then
+            f.x = f.x - step
+        end
+        if direction == 'right' then
+            f.x = f.x + step
+        end
+        w:setFrame(f)
+    end
+
+    local mapping = {
+        up = 'k',
+        down = 'j',
+        left = 'h',
+        right = 'l',
     }
+    cmodal:bind('ctrl+alt', '/', function()
+        obj:_toggleHelp(mapping)
+    end)
+
     for k, v in pairs(mapping) do
-        if k == 'help' then hs.hotkey.bind(v[1], v[2], def[k]) end
+        cmodal:bind('', v, function()
+            move(k)
+        end)
     end
-    return self
 end
 
-function obj:_setupReloadConfig()
-    local function notifyAndReload()
-        hs.notify.new({title="Hammerspoon", informativeText="Config reloaded."}):send()
-        hs.reload() 
+function obj:_toggleHelp(data)
+    local function create()
+        local mainRes = hs.screen.primaryScreen():fullFrame()
+        local helpCanvas = hs.canvas.new({ x = (mainRes.w - 600) / 2, y = (mainRes.h - 700) / 2, w = 600, h = 700 }):
+            appendElements(
+                {
+                    type = "rectangle",
+                    action = "fill",
+                    fillColor = { hex = "#EEEEEE", alpha = 0.95 },
+                    roundedRectRadii = { xRadius = 10, yRadius = 10 },
+                },
+                {
+                    type = 'text',
+                    text = hs.inspect(data or obj.map) .. '\n\n 😄',
+                    textSize = 20,
+                    textColor = { hex = "#2390FF", alpha = 1 },
+                    textAlignment = 'left',
+                }
+            )
+        return helpCanvas
     end
 
-    obj.daemons['cfgWatcher'] = hs.pathwatcher.new(hs.configdir, notifyAndReload):start()
+    local helpCanvas = obj.canvases['helpCanvas']
+
+    if not helpCanvas then
+        helpCanvas = create()
+        obj.canvases['helpCanvas'] = helpCanvas
+    end
+    if helpCanvas:isShowing() then
+        helpCanvas:hide()
+    else
+        helpCanvas:show()
+    end
 end
 
 function obj:_toggleClock(bool)
-    local ctimer = obj.daemons['ctimer']
-    if ctimer and ctimer:running() then
-        if not bool then
-            ctimer:stop()
-            ctimer = nil
-            obj.canvases['ccanvas']:hide()
-        end
-    elseif bool ~= false then
+    local function create()
         local mainRes = hs.screen.primaryScreen():fullFrame()
-        local ccanvas = hs.canvas.new({x=0, y=mainRes.h - 60*2, w=200, h=60*2}):appendElements(
+        local clockCanvas = hs.canvas.new({x=0, y=mainRes.h - 60*2, w=200, h=60*2}):appendElements(
             {
                 type = 'rectangle',
                 action = 'fill',
@@ -133,64 +230,34 @@ function obj:_toggleClock(bool)
                 frame = {x=10, y=60, w=120, h=60}
             }
         )
-        ccanvas:bringToFront(true)
-        ccanvas:behavior(hs.canvas.windowBehaviors.canJoinAllSpaces)
-        ccanvas:show()
-        obj.canvases['ccanvas'] = ccanvas
-        ctimer = hs.timer.doEvery(20, function()
-            ccanvas[2].text = os.date('%a %b %e')
-            ccanvas[3].text = os.date('%H:%M')
-        end)
-        obj.daemons['ctimer'] = ctimer
+        clockCanvas:bringToFront(true)
+        clockCanvas:behavior(hs.canvas.windowBehaviors.canJoinAllSpaces)
+        return clockCanvas
     end
-end
 
-function obj:_setupAppLauncher()
-    local id = 'appLauncher'
-    local keys = obj.cfg.keymap[id]
-    local cmodal = hs.hotkey.modal.new(keys[1], keys[2], string.format('Entered %s mode', id))
-    -- function cmodal:entered() hs.alert(string.format('Entered %s mode', id)) end
-    -- function cmodal:exited()  hs.alert(string.format('Exited %s mode', id))  end
-    cmodal:bind('', 'escape', string.format('Exited %s mode', id), function() cmodal:exit() end)
+    local clockCanvas = obj.canvases['clockCanvas']
+    local clockTimer = obj.timers['clockTimer']
 
-    cmodal:bind('alt', '/', function ()
-        obj:_toggleHelp(obj.cfg['appmap'])
-    end)
-
-    for k, v in pairs(obj.cfg['appmap']) do
-        cmodal:bind('', k, function()
-            hs.application.launchOrFocus(v)
+    if not clockCanvas then
+        clockCanvas = create()
+        obj.canvases['clockCanvas'] = clockCanvas
+        clockTimer = hs.timer.doEvery(20, function()
+            clockCanvas[2].text = os.date('%a %b %e')
+            clockCanvas[3].text = os.date('%H:%M')
         end)
+        obj.timers['clockTimer'] = clockTimer
     end
-end
-
-function obj:_toggleHelp(data)
-    local hcanvas = obj.canvases['hcanvas']
-    if hcanvas and hcanvas:isShowing() then
-        hcanvas:hide()
+    if clockCanvas:isShowing() then
+        if not bool then
+            clockCanvas:hide()
+        end
     else
-        local mainRes = hs.screen.primaryScreen():fullFrame()
-        local hcanvas = hs.canvas.new({x=(mainRes.w-600)/2, y=(mainRes.h-700)/2, w=600, h=700}):appendElements(
-            {
-                type = "rectangle",
-                action = "fill",
-                fillColor = {hex = "#EEEEEE", alpha = 0.95},
-                roundedRectRadii = {xRadius = 10, yRadius = 10},
-            },
-            {
-                type = 'text',
-                text = hs.inspect(data or obj.cfg.keymap) .. '\n\n 😄',
-                textSize = 20,
-                textColor = {hex = "#2390FF", alpha = 1},
-                textAlignment = 'left',
-            }
-        )
-        hcanvas:show()
-        obj.canvases['hcanvas'] = hcanvas
+        if bool ~= false then
+            clockCanvas:show()
+        end
     end
 end
 
-
---========== main ==========--
-obj:init():bindHotkeys(obj.cfg.keymap):start()
+---- main
+obj:init():bindHotkeys(obj.map):start()
 return obj
